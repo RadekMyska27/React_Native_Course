f6c5d720ea2b39875c80d02445f1cd3e6182d5ec
r.myska <r.myska@quadient.com>
2023-12-29 11:02:51 +0100

    navigation install

--- App.tsx
+++ App.tsx
@@ -2,10 +2,6 @@ import {StatusBar, StyleSheet} from 'react-native';
 import React from "react";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
-export interface IGoal {
-    text: string,
-    id: number
-}
 
 export default function App() {
 

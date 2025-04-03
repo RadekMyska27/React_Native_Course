c31e105285ffcc3611c494be297ef5ad9ba018d9
radek.myska <r.myska@quadient.com>
2025-04-03 12:14:45 +0200

    file picker

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import {Button, FlatList, Modal, StyleSheet, View} from 'react-native';
+import {Button, FlatList, Image, Modal, StyleSheet, View} from 'react-native';
 import React, {useState} from "react";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
+import ImagePicker from "./components/ImagePicker";
 
 export interface IGoal {
     text: string,
@@ -11,6 +12,7 @@ export interface IGoal {
 export default function App() {
     const [goals, setGoals] = useState<IGoal[]>([])
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isFileExplorer, setIsFileExplorer] = useState(false)
 
     function addGoalHandler(goal: string) {
         const generatedKey = Math.random()
@@ -28,36 +30,44 @@ export default function App() {
             return currentGoals.filter(g => g.id !== id)
         })
     }
-    
+
     function onCancelAddGoalHandler() {
         setIsModalOpen(false)
     }
 
+    function onCancelFileExplorerHandler() {
+        setIsFileExplorer(false)
+    }
+
     return (
-        <View style={styles.appContainer}>
-            <Button title={"Add new goal"} onPress={() => setIsModalOpen(true)}></Button>
+            <View style={styles.appContainer}>
+                {/*<Button title={"Add Task"} onPress={() => setIsModalOpen(true)}></Button>*/}
 
-            <Modal visible={isModalOpen} animationType={"slide"} onRequestClose={onCancelAddGoalHandler}>
-                <GoalInput addGoalHandler={addGoalHandler} onCancel={onCancelAddGoalHandler}/>
-            </Modal>
+                <ImagePicker/>
 
-            <View style={styles.goalContainer}>
-                {/* first option add scroll list - this is not effective for list because all memmbers are rendered even if member is not visible*/}
+                {/*<Modal visible={isModalOpen} animationType={"slide"} onRequestClose={onCancelAddGoalHandler}>*/}
+                {/*    <GoalInput addGoalHandler={addGoalHandler} onCancel={onCancelAddGoalHandler}/>*/}
+                {/*</Modal>*/}
 
-                {/*<ScrollView>*/}
-                {/*    {goals.map((g, index) => <Text key={index}>{g}</Text>)}*/}
-                {/*</ScrollView>*/}
 
-                {/* second option of add scroll list*/}
 
-                <FlatList
-                    data={goals}
-                    renderItem={(itemData) => {
-                        return <GoalItem goal={itemData.item} removeGoalHandler={removeGoalHandler}/>
-                    }}
-                />
+                {/*<View style={styles.goalContainer}>*/}
+                {/*    /!* first option add scroll list - this is not effective for list because all memmbers are rendered even if member is not visible*!/*/}
+
+                {/*    /!*<ScrollView>*!/*/}
+                {/*    /!*    {goals.map((g, index) => <Text key={index}>{g}</Text>)}*!/*/}
+                {/*    /!*</ScrollView>*!/*/}
+
+                {/*    /!* second option of add scroll list*!/*/}
+
+                {/*    <FlatList*/}
+                {/*            data={goals}*/}
+                {/*            renderItem={(itemData) => {*/}
+                {/*                return <GoalItem goal={itemData.item} removeGoalHandler={removeGoalHandler}/>*/}
+                {/*            }}*/}
+                {/*    />*/}
+                {/*</View>*/}
             </View>
-        </View>
     );
 }
 
@@ -70,5 +80,8 @@ const styles = StyleSheet.create({
     },
     goalContainer: {
         flex: 4
+    },
+    button: {
+        marginTop: 5
     }
 });
---
+++ components/ImagePicker.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import {Button, StyleSheet, View, Text, Image, Dimensions} from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+
+
+export interface IImagePickerData {
+
+}
+
+const ImagePicker = (data: IImagePickerData) => {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const {width, height} = Dimensions.get("window"); // Get screen width and height
+    const MARGIN = 20; // Define your margin
+
+    async function pickImage() {
+        try {
+            const result = await DocumentPicker.getDocumentAsync({
+                type: "image/*", // Allows selecting only images
+                copyToCacheDirectory: false,
+            });
+
+            if (result.canceled) {
+                return
+            } else {
+                setSelectedImage(result.assets[0].uri);
+            }
+        } catch (error) {
+            console.error("Error picking document:", error);
+        }
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        buttonContainer: {
+            marginTop: 10, // Adjust spacing between image & button
+            width: "80%", // Make the button width responsive
+        },
+        image: {
+            width: width - MARGIN * 2, // Max width minus margin
+            height: height/2, // Max height minus margin
+            resizeMode: "contain", // Ensures the image scales properly
+        }
+    });
+
+
+    return (
+            <View style={styles.container}>
+                {selectedImage && (
+                        <Image source={{uri: selectedImage}} style={styles.image}/>
+                )}
+                <View style={styles.buttonContainer}>
+                    <Button title="Pick an Image" onPress={pickImage}/>
+                </View>
+            </View>
+    )
+}
+
+
+export default ImagePicker
